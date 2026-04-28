d30058e06d17222f234c68391fdd93a6ebbec7df
gurender.singh <gurender.singh@ForeFlights-MacBook-Pro-2.local>
2026-04-28 18:17:29 +0530

    Integrated tavily search

--- main.py
+++ main.py
@@ -9,22 +9,12 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 #package to create a language model
 from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
 
 
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search results
-    """
-    print(f"Searching for: {query}")
-    return "Tokyo weather is sunny"
 
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 def main():
